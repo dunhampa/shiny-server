c23eb29ab0d9abbc967d7a364b13a873c1be4f02
Peter Dunham <dunhampa@gmail.com>
2018-08-21 14:18:47 -0400

    adding file for faster load

--- pete-test/shiny_ohio/app.R
+++ pete-test/shiny_ohio/app.R
@@ -43,46 +43,14 @@ ui <- fluidPage(
 # Server logic
 server <- function(input, output) {
   
-  birthdata<-read.csv("ReportOutput-BirthWeight.csv", stringsAsFactors = FALSE)
   
   
   
-  myMap <-function(){
-    print("here")
-    states <- readOGR("county/cb_2015_us_county_20m.shp",
-                      layer = "cb_2015_us_county_20m", GDAL1_integer64_policy = TRUE)
-    
-    Statekey<-read.csv('STATEFPtoSTATENAME_Key.csv', colClasses=c('character'))
-    states<-merge(x=states, y=Statekey, by="STATEFP", all=TRUE)
-    
-    
-    neStates <- subset(states, states$STATENAME %in% c(
-      "Ohio"
-    ))
-    
-    neStates<-sp::merge(x=neStates, y=birthdata, by.x="NAME", by.y="County", by=x)
-    
-   #Low.Birth<-as.numeric(birthdata$Low.Birth.Count)[1:88]
-   #neStates<-cbind(neStates,Low.Birth)
-    #states<-merge(x=states, y=Statekey, by="STATEFP", all=TRUE)
-    map<-leaflet(neStates) %>%
-      addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
-                  opacity = 1.0, fillOpacity = 0.5,
-                  layerId = ~NAME,
-                  fillColor = ~colorQuantile("Reds",Low.Birth.Count )(Low.Birth.Count),
-                  highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                      bringToFront = TRUE),
-                  popup = ~as.factor(paste0(neStates$NAME,",",neStates$Low.Birth.Count,",",neStates$Norm.Birth.Count)))
-
-    print("here")
-    #setView(11, 49,  zoom = 9)
-    #map$center<-list(lng=-82.7, lat=35.7)
-    map<-map %>% setView(-82, 38.9,  zoom = 7)
-  }
+  
+ 
   
   output$myMap <- renderLeaflet({
-    #print(str(myMap()))
-    myMap()
+   readRDS("map.RDS")
   })
   
   
